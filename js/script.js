1f401fd5e93837363c5dc69eb07daaa461fb9e2c
Wilker Sousa <wilker.sou26@gmail.com>
2021-05-30 17:48:10 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+//animação de hamburquer em telas mobile na navbar
+
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+//voltar para o topo com animação de pulo em jquery
+
+$(function(){
+  $(document).on( 'scroll', function(){
+      if ($(window).scrollTop() > 100) {
+          $('.smoothscroll-top').addClass('show');
+      } else {
+          $('.smoothscroll-top').removeClass('show');
+      }
+  });
+  $('.smoothscroll-top').on('click', scrollToTop);
+});
+
+function scrollToTop() {
+  verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+  element = $('body');
+  offset = element.offset();
+  offsetTop = offset.top;
+  $('html, body').animate({scrollTop: offsetTop}, 600, 'linear').animate({scrollTop:25},200).animate({scrollTop:0},150) .animate({scrollTop:0},50);
+}
